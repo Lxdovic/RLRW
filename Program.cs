5241b4294b532706d021a2a597d2fb873e55d64b
ludovic <ludovicdebever0@gmail.com>
2024-06-16 19:07:30 +0200

    chore: init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Raylib_cs;
+using rlImGui_cs;
+using RLReplayWatcher.ui;
+using RLReplayWatcher.ui.scene;
+using RocketLeagueReplayParser;
+
+namespace RLReplayWatcher;
+
+internal class Program {
+    private const int ScreenHeight = 780;
+    private const int ScreenWidth = 1280;
+    internal static Replay? Replay;
+    internal static Scene? Scene;
+
+    private static void Main(string[] args) {
+        Raylib.SetWindowState(ConfigFlags.ResizableWindow);
+        Raylib.InitWindow(ScreenWidth, ScreenHeight, "RLRW");
+
+        Theme.SetupFonts();
+        rlImGui.Setup(true, true);
+        Theme.ApplyTheme();
+        Raylib.SetExitKey(KeyboardKey.Null);
+
+        while (!Raylib.WindowShouldClose()) Ui.Render();
+
+        Scene?.Unload();
+
+        rlImGui.Shutdown();
+        Raylib.CloseWindow();
+    }
+}
