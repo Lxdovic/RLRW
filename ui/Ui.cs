fc66a859935f9ea565fc5f5d6a91b5c798bff08e
ludovic <ludovicdebever0@gmail.com>
2024-06-16 19:08:52 +0200

    feat(ui) basic layout

---
+++ ui/Ui.cs
@@ -0,0 +1,59 @@
+using System.Numerics;
+using ImGuiNET;
+using NativeFileDialogSharp;
+using Raylib_cs;
+using rlImGui_cs;
+using RLReplayWatcher.ui.scene;
+using RocketLeagueReplayParser;
+
+namespace RLReplayWatcher.ui;
+
+internal static class Ui {
+    internal static void Render() {
+        rlImGui.Begin();
+        Raylib.BeginDrawing();
+
+        ImGui.PushFont(Theme.Fonts["Poppins-Regular"]);
+
+        ImGui.SetNextWindowPos(Vector2.Zero, ImGuiCond.Always);
+        ImGui.SetNextWindowSize(new Vector2(Raylib.GetScreenWidth(), Raylib.GetScreenHeight()), ImGuiCond.Always);
+        ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, 0);
+        ImGui.PushStyleVar(ImGuiStyleVar.WindowRounding, 0);
+
+        ImGui.ShowDemoWindow();
+
+        ImGui.Begin("RLRW", ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove |
+                            ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoScrollbar |
+                            ImGuiWindowFlags.NoScrollWithMouse |
+                            ImGuiWindowFlags.NoBringToFrontOnFocus | ImGuiWindowFlags.NoNavFocus |
+                            ImGuiWindowFlags.NoNav);
+        ImGui.PopStyleVar(2);
+        
+        ImGui.BeginChild("Left Menu", new Vector2(400, ImGui.GetWindowHeight()),
+            ImGuiChildFlags.ResizeX | ImGuiChildFlags.AlwaysUseWindowPadding);
+
+        if (ImGui.Button("Parse")) {
+            var result = Dialog.FileOpen("replay");
+
+            Program.Replay = Replay.Deserialize(result.Path);
+            Program.Scene = new Scene();
+        }
+        
+        ImGui.EndChild();
+        ImGui.SameLine();
+        ImGui.BeginChild("Scene", new Vector2(ImGui.GetColumnWidth() / 2, ImGui.GetWindowHeight()),
+            ImGuiChildFlags.AlwaysUseWindowPadding | ImGuiChildFlags.ResizeX);
+
+        Program.Scene?.Update();
+        Program.Scene?.Render();
+
+        ImGui.EndChild();
+
+        ImGui.End();
+
+        ImGui.PopFont();
+
+        Raylib.EndDrawing();
+        rlImGui.End();
+    }
+}
---
+++ ui/scene/Scene.cs
@@ -0,0 +1,51 @@
+using System.Numerics;
+using ImGuiNET;
+using Raylib_cs;
+using rlImGui_cs;
+
+namespace RLReplayWatcher.ui.scene;
+
+internal sealed class Scene {
+    internal Camera3D Camera;
+    internal bool IsGrabbed;
+    internal RenderTexture2D ViewTexture;
+
+    internal Scene() {
+        ViewTexture = Raylib.LoadRenderTexture(Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
+
+        Camera.FovY = 45;
+        Camera.Up.Y = 1;
+        Camera.Position.Y = 3;
+        Camera.Position.Z = -25;
+    }
+
+    internal void Render() {
+        rlImGui.ImageRenderTextureFit(ViewTexture);
+    }
+
+    internal void Update() {
+        if (IsGrabbed) HandleControls();
+
+        Raylib.BeginTextureMode(ViewTexture);
+        Raylib.ClearBackground(Color.Green);
+        Raylib.BeginMode3D(Camera);
+        Raylib.DrawPlane(new Vector3(0, 0, 0), new Vector2(50, 50), Color.Beige); // simple world plane
+
+        Raylib.EndMode3D();
+        Raylib.EndTextureMode();
+    }
+
+    private void HandleControls() {
+        Raylib.UpdateCamera(ref Camera, CameraMode.FirstPerson);
+
+        if (Raylib.IsMouseButtonPressed(MouseButton.Left)) IsGrabbed = true;
+        if (Raylib.IsKeyPressed(KeyboardKey.Escape)) IsGrabbed = false;
+        
+        if (IsGrabbed) Raylib.DisableCursor();
+        if (!IsGrabbed) Raylib.EnableCursor();
+    }
+
+    internal void Unload() {
+        Raylib.UnloadRenderTexture(ViewTexture);
+    }
+}
