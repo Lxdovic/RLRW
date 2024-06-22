b4bdb089643ceac70c15a545eb41e7859cd0c215
ludovic <ludovicdebever0@gmail.com>
2024-06-22 12:15:08 +0200

    chore(ui): removed props explorer as it is now useless

--- ui/Ui.cs
+++ ui/Ui.cs
@@ -11,10 +11,6 @@ namespace RLReplayWatcher.ui;
 
 internal static class Ui {
     private static string? _path;
-    private static int _frameIndex;
-    private static int _objId;
-    private static int _nameId;
-    private static string _notes = "";
 
     internal static void Render() {
         rlImGui.Begin();
@@ -52,53 +48,13 @@ internal static class Ui {
             Program.Game = new GameManager(Program.Replay);
         }
 
-        ImGui.InputTextMultiline("Notes", ref _notes, 1000, new Vector2(ImGui.GetColumnWidth(), 100));
-
         ImGui.EndChild();
         ImGui.SameLine();
-        ImGui.BeginChild("PropsExplorer", new Vector2(ImGui.GetColumnWidth(), ImGui.GetWindowHeight()),
+        ImGui.BeginChild("Scene", new Vector2(ImGui.GetColumnWidth(), ImGui.GetWindowHeight()),
             ImGuiChildFlags.AlwaysUseWindowPadding);
 
         Program.Scene?.Update();
         Program.Scene?.Render();
-
-        ImGui.InputInt("Object Id", ref _objId);
-        ImGui.InputInt("Name Id", ref _nameId);
-
-        _nameId = Math.Clamp(_nameId, 0, Program.Replay?.Names.Length - 1 ?? 0);
-        _objId = Math.Clamp(_objId, 0, Program.Replay?.Objects.Length - 1 ?? 0);
-
-        ImGui.Text($"Object: {Program.Replay?.Objects[_objId]}");
-        ImGui.Text($"Name: {Program.Replay?.Names[_nameId]}");
-
-        ImGui.InputInt("Frame Index", ref _frameIndex);
-
-        for (var i = 0; i < Program.Replay?.Frames[_frameIndex].ActorStates.Count; i++) {
-            var actor = Program.Replay.Frames[_frameIndex].ActorStates[i];
-
-            if (ImGui.CollapsingHeader($"{i}")) {
-                ImGui.SeparatorText("Ids");
-
-                ImGui.Text($"Id {actor.Id}");
-                ImGui.Text($"NameId {actor.NameId}");
-                ImGui.Text($"TypeId {actor.TypeId}");
-                ImGui.Text($"ClassId {actor.ClassId}");
-
-                ImGui.SeparatorText("Properties");
-
-                ImGui.Text($"Name?: {ReplayHelper.GetName(Program.Replay, actor)}");
-                ImGui.Text($"Class?: {ReplayHelper.GetClass(Program.Replay, actor)?.Class}");
-
-                foreach (var (_, prop) in actor.Properties) ImGui.Text($"{prop.PropertyName}: {prop.Data}");
-
-                ImGui.SeparatorText("Other Props");
-
-                ImGui.Text($"Position: {actor.Position}");
-                ImGui.Text($"Rotation: {actor.Rotation}");
-                ImGui.Text($"Unknown1: {actor.Unknown1}");
-                ImGui.Text($"State: {actor.State}");
-            }
-        }
 
         ImGui.EndChild();
 
